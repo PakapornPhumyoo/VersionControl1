326d24d21ed910b6c5be4d3fdae6c5dda131ef0d
PakapornPhumyoo <littlecutecumber27@gmail.com>
2025-07-16 15:16:35 +0700

    Edit notify in quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -87,7 +87,10 @@ export default defineConfig((ctx) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify',
+
+      ]
     },
 
     // animations: 'all', // --- includes all animations
